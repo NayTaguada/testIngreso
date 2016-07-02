3f641eb7ca93205d20f317fee0e6b06100f9bd74
naytaguada <naylalucia@gmail.com>
2016-07-01 22:03:02 -0300

    Mas edicion

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,14 +5,17 @@ en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
 
-	var sueldo = 100;
+	var sueldo;
 	var resultado;
 
 	sueldo = document.getElementById('sueldo').value;
 	sueldo = parseInt(sueldo);
 
 	resultado = sueldo * 1.1;
-	alert(resultado);
+	
+	
 	
 	// como es un aumento es multiplicado, es * 1.1 
+
+	// (git gui) 1. github-crear 2.clonar 3.github, hd 
 }
